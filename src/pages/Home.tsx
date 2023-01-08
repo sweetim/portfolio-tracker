abc625d32329eacb9e3f85fa2af62f5f1b260a99
sweetim <hosweetim@gmail.com>
2023-01-08 16:46:13 +0900

    added router at home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,43 @@
-import { Layout, Input, Button, Statistic } from 'antd';
-
-import { CheckOutlined } from '@ant-design/icons';
 import { useState } from 'react';
+
+import { Layout, Input, Button, Statistic } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
+
+import { useNavigate } from 'react-router-dom';
 
 import { parseRakutenRawData } from '../parser/rakuten'
 
 function Home() {
+    const DEFAULT_TEXT = ``
+
     const { Content } = Layout
     const { TextArea } = Input;
 
-    const [rawData, setRawData] = useState('')
+    const [ rawData, setRawData ] = useState(DEFAULT_TEXT)
     const [ numberOfCompanies, setNumberOfCompanies ] = useState(0)
+
+    const navigate = useNavigate()
 
     const submitButtonHandler = () => {
         const data = parseRakutenRawData(rawData)
-
         setNumberOfCompanies(Object.keys(data).length)
+
+        navigate('/app', {
+            state: data
+        })
 
         console.log(parseRakutenRawData(rawData))
     }
 
     const textAreaChangeHandler = (e) => {
+        console.log('here')
         setRawData(e.target.value)
+
+        const data = parseRakutenRawData(e.target.value)
+        setNumberOfCompanies(Object.keys(data).length)
     }
+
+
 
     return (
         <Layout style={{ height: "100vh" }}>
@@ -31,6 +46,7 @@ function Home() {
                 <TextArea
                     autoSize={{ minRows: 6, maxRows: 30 }}
                     bordered={false}
+                    value={DEFAULT_TEXT}
                     placeholder="paste the table here"
                     onChange={textAreaChangeHandler} />
 
