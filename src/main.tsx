9d651cb228b7c2abd4af62fec4f8f9e2da35d5db
sweetim <hosweetim@gmail.com>
2023-01-17 01:36:44 +0900

    added usequery

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from 'react-router-dom';
 
 import { Home, App } from './pages'
@@ -10,25 +10,32 @@ import { Home, App } from './pages'
 import 'antd/dist/reset.css';
 
 import { ConfigProvider, theme } from 'antd';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/app',
-    element: <App />
-  }
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/app',
+        element: <App />
+    }
 ])
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-      }}>
-      <RouterProvider router={router} />
-    </ConfigProvider >
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ConfigProvider
+            theme={{
+                algorithm: theme.darkAlgorithm,
+            }}>
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ConfigProvider >
+    </React.StrictMode>,
 )
