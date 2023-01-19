dbdbdabe5b7d32fedba7b407f13e137f12e9d979
sweetim <hosweetim@gmail.com>
2023-01-19 20:34:38 +0900

    updated script for password protected page

--- __checks__/portfolio-tracker.check.js
+++ __checks__/portfolio-tracker.check.js
@@ -5,10 +5,31 @@
  */
 const { expect, test } = require('@playwright/test')
 
+
+const req = require('request-promise')
+
+
+
 test('visit page and take screenshot', async ({ page }) => {
+  const vercelDeploymentPassword = process.env.PASSWORD_VERCEL
+
   // If available, we set the target URL to a preview deployment URL provided by the ENVIRONMENT_URL created by Vercel.
   // Otherwise, we use the Production URL.
   const targetUrl = process.env.ENVIRONMENT_URL || 'https://pt.timx.co'
+
+  const options = {
+    uri: targetUrl,
+    simple: false,
+    resolveWithFullResponse: true
+  };
+  try {
+    const response = await req.post(options).form({ _vercel_password: vercelDeploymentPassword })
+    const token = response.headers['set-cookie']
+    const tokenString = token.toString().split(';')[0]
+    request.headers['Cookie'] = tokenString
+  } catch (error) {
+    console.log(error)
+  }
 
   // We visit the page. This waits for the "load" event by default.
   const response = await page.goto(targetUrl)
