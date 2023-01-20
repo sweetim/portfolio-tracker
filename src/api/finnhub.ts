be5814a7cd892fa1abd4ad1dc92d05cc06a15357
sweetim <hosweetim@gmail.com>
2023-01-21 01:25:22 +0900

    added finnhub ws types

--- src/api/finnhub.ts
+++ src/api/finnhub.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {  StockProfile, StockQuote } from "@/model/stocks"
 
 const FINNHUB_API_URL = "https://finnhub.io/api/v1"
+const FINNHUB_WSS_URL = "wss://ws.finnhub.io"
 const FINNHUB_API_TOKEN = "cets3eqad3i5jsala8g0cets3eqad3i5jsala8gg"
 
 const axiosInstance = axios.create({
@@ -17,6 +18,10 @@ axiosInstance.interceptors.request.use((config) => {
 
     return config
 })
+
+export function getFinnHubWSSUrl() {
+    return `${FINNHUB_WSS_URL}?token=${FINNHUB_API_TOKEN}`
+}
 
 export async function getStockProfile(symbol: string): Promise<StockProfile> {
     const { data } = await axiosInstance.get(`/stock/profile2?symbol=${symbol}`)
@@ -62,4 +67,34 @@ export async function getStockQuote(symbol: string): Promise<StockQuote> {
         openPrice,
         previousClose
     }
+}
+
+export type WSSendType = "subscribe"
+export type WSReceiveType = "ping"
+    | "trade"
+
+export interface WSSendData {
+    type: string
+    symbol: string
+}
+
+export interface WSReceiveTrade {
+    symbol: string
+    price: number
+    timestamp_ms: number
+    volume: number
+    condition: string[]
+}
+
+export interface WSReceiveRawTrade {
+    s: string
+    p: number
+    v: number
+    t: number
+    c: string[]
+}
+
+export interface WSReceiveRawData {
+    data?: WSReceiveRawTrade[]
+    type: WSReceiveType
 }
